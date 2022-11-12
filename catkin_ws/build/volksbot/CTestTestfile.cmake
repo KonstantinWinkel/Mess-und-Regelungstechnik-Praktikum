c6731e153905766b19aaa3be5871211e807aa1af
FyremanMusic <winkel.konstantin@gmail.com>
2022-11-12 15:10:55 +0100

    deleted unneeded directories
    those directories can be generated with catkin_make

--- catkin_ws/build/volksbot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fyreman/catkin_ws/src/volksbot
-# Build directory: /home/fyreman/catkin_ws/build/volksbot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/volksbot/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/fyreman/catkin_ws/devel/include;/home/fyreman/catkin_ws/src/volksbot/include".split(';') if "/home/fyreman/catkin_ws/devel/include;/home/fyreman/catkin_ws/src/volksbot/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;message_runtime;roscpp;geometry_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "volksbot"
-PROJECT_SPACE_DIR = "/home/fyreman/catkin_ws/devel"
-PROJECT_VERSION = "0.0.1"
